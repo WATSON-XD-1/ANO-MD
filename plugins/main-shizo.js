7c1268c120afd1ed51ab022310b41bf107e62b6d
WATSON-XD-1 <watsonfourpence209@gmail.com>
2024-09-04 07:41:31 +0200

    Update main-shizo.js

--- plugins/main-shizo.js
+++ plugins/main-shizo.js
@@ -2,32 +2,32 @@ let handler = async m =>
   m.reply(
     `
 
-≡ 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 GROUPS
+≡ ANO 𝐌𝐃 𝐁𝐎𝐓 GROUPS
 
 ─────────────
 ▢ Join public bot group and support
-https://chat.whatsapp.com/Jjj2lYrtGHc5WY2rUmC6JD
+https://chat.whatsapp.com/E0a2bl9wHYlCHuL35WBR88
 
 ▢ Group 
-https://chat.whatsapp.com/Lr80ac3MKKIKGew8mFES2a
+ https://chat.whatsapp.com/E0a2bl9wHYlCHuL35WBR88
 
 ─────────────
 ≡ Disabled links? enter here! 
 
 ▢ Channel WhatsApp 
- https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v
+ https://whatsapp.com/channel/0029VajjzuB9sBI890YffB1b
 ─────────────
 ▢ *Owner instagram*
- https://instagram.com/_its.silva
+ https://instagram.com/tinashexd
 
 ▢ *YouTube*
-• https://www.youtube.com/@silvaedits254
+• https://www.youtube.com/@watsonFOURPENCE-hq9ks
 
 
 `.trim()
   )
 handler.help = ['ruth']
 handler.tags = ['main']
-handler.command = ['groups', 'groupsilva', 'silvagp', 'sgp', 'grp']
+handler.command = ['groups', 'groupano', 'anogp', 'sgp', 'group']
 
 export default handler
