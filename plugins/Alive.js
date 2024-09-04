135c9f848bf1ecce552297bd3eed61b9679450f2
WATSON-XD-1 <watsonfourpence209@gmail.com>
2024-09-04 08:08:48 +0200

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender)
   var vn = 'https://cdn.jsdelivr.net/gh/SilvaTechB/silva-md-bot@main/media/Alive.mp3'
-  let url = 'https://github.com/SilvaTechB/silva-md-bot'
+  let url = 'https://github.com/WATSON-XD1/ANO-MD'
   let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e'
   let img = 'https://i.imgur.com/QMyKIPq.jpeg'
   let con = {
@@ -30,10 +30,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 ALIVE',
-        body: '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓',
+        title: 'ANO MD BOT ALIVE',
+        body: 'ANO MD BOT',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v',
+        sourceUrl: 'https://whatsapp.com/channel/0029VajjzuB9sBI890YffB1b',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
