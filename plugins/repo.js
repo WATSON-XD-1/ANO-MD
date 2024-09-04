c521c4e6bf21eb7a4eb3fa77f2effee80b761adc
WATSON-XD-1 <watsonfourpence209@gmail.com>
2024-09-04 07:35:44 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -3,35 +3,35 @@ import { join } from 'path';
 import axios from 'axios'; 
 
 let handler = async function (m, { conn, __dirname }) {
-const githubRepoURL = 'https://github.com/SilvaTechB/silva-md-bot';
+const githubRepoURL = 'https://github.com/WATSON-XD1/ANO-MD';
   try {
 const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
 const response = await axios.get(`https://api.github.com/repos/${username}/${repoName}`);
 if (response.status === 200) {
 const repoData = response.data;
 const formattedInfo = `
-🍑🍆𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓💦☣
+🍑🍆ANO 𝐌𝐃 𝐁𝐎𝐓💦☣
 *This is a Horny WhatsApp Bot Loaded with Features 🎊*
 ┃✰𝖲𝖳𝖠𝖱✨ :: ${repoData.stargazers_count}
 ┃✰𝖥𝖮𝖱𝖪 🍽️ :: ${repoData.forks_count}
 ┃✰𝖴𝖱𝖫 👻 :: ${repoData.html_url}
 \`🚀 OUR REPOSITORY\`
-*Welcome to Silva MD! 🤖✨*
+*Welcome to ANO MD! 🤖✨*
 
-_Get Started: Welcome to the Silva MD WhatsApp bot repository! 🎉 Feel free to fork this project and customize it for your own WhatsApp experience._
+_Get Started: Welcome to the Ano MD WhatsApp bot repository! 🎉 Feel free to fork this project and customize it for your own WhatsApp experience._
 
 *How to Use:*
 
 1. *Fork the Repository:* Click on the “Fork” button to create your own copy.
 2. *Set Up Your Bot:* Follow the setup instructions in the README file.
-3. *Enjoy Your Bot:* Start chatting with Silva MD and explore its features! 💬
+3. *Enjoy Your Bot:* Start chatting with ANO MD and explore its features! 💬
 
 Thank you for joining our community! If you have any questions, don’t hesitate to reach out. Happy coding! 🚀
 > Trying to make it a user friendly bot
 
-*_DEPLOY 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 NOW_*
+*_DEPLOY ANO 𝐌𝐃 𝐁𝐎𝐓 NOW_*
 
-\`\`\`USER FRIENDLY 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓💥\`\`\`
+\`\`\`USER FRIENDLY ANO 𝐌𝐃 𝐁𝐎𝐓💥\`\`\`
         `.trim();
 
       await conn.relayMessage(m.chat,  {
@@ -57,6 +57,6 @@ Thank you for joining our community! If you have any questions, don’t hesitate
 };
 handler.help = ['script'];
 handler.tags = ['main'];
-handler.command = ['sc', 'repo', 'script', 'code', 'silva'];
+handler.command = ['sc', 'repo', 'script', 'code', 'xd'];
 
 export default handler;
